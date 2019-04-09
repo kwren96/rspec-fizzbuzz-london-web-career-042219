87a141e4e5bdf9798b27b6af20588ad7e15cd327
Katherine Wren <kwren@outlook.com>
2019-04-09 11:58:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  fizz_3 = "Fizz"
-  fizz_5 = "Buzz"
-  fizz_15 = "FizzBuzz"
  if int % 15 == 0 
-   puts fizz_15
+   puts "FizzBuzz"
   elsif int % 5 == 0 
-    puts fizz_5
+    puts "Fizz"
    elsif int % 3 == 0 
-   puts fizz_3
+   puts "Buzz"
  else 
    puts nil
  end 
